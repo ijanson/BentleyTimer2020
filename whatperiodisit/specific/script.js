94c6ae93bb28f3bf2f44bee30a049bb792250708
JeremyHub <45571333+JeremyHub@users.noreply.github.com>
2019-03-15 21:57:02 -0700

    added support for new names in spreadsheet

--- whatperiodisit/specific/script.js
+++ whatperiodisit/specific/script.js
@@ -7,10 +7,10 @@ function whatPeriod(data) {
 	var w = d.getDay();
 	var s = d.getSeconds();
 	var p = "Nice, it's not school right now!";
-	var n = "ERROR!! var n was not changed";
+	var n = "ERROR!! var n was not changed (for next period)";
 	var t = (h * 3600) + (m * 60) + s; //seconds since midnight
 	var data = data.feed.entry;
-	currentpersonnumber = getpersonnumber(document.getElementById('currentpersoninput').value);
+	cpn = getpersonnumber(document.getElementById('currentpersoninput').value); //current person number
 
 	function time (timeinput){
 		var string = timeinput.split(':', 2);
@@ -20,8 +20,8 @@ function whatPeriod(data) {
 		return total;
 	}
 
-	function getperiod (person, period) {
-		var allperiods = data[person]['content']['$t'];
+	function getperiod (personnumber, period) {
+		var allperiods = data[personnumber]['content']['$t'];
 		var eachperiod = allperiods.split(',');
 		var finalperiod = eachperiod[period - 1].trim().slice(4 , allperiods.length);
 		return finalperiod;
@@ -29,103 +29,96 @@ function whatPeriod(data) {
 
 	function getpersonnumber (directinput) {
 		var input = directinput.trim();
-		if (input == data[0]['title']['$t']) {return 0}
-		else if (input == data[1]['title']['$t']) {return 1}
-		else if (input == data[2]['title']['$t']) {return 2}
-		else if (input == data[3]['title']['$t']) {return 3}
-		else if (input == data[4]['title']['$t']) {return 4}
-		else if (input == data[5]['title']['$t']) {return 5}
-		else if (input == data[6]['title']['$t']) {return 6}
-		else if (input == data[7]['title']['$t']) {return 7}
-		else if (input == data[8]['title']['$t']) {return 8}
-		else if (input == data[9]['title']['$t']) {return 9}
-		else if (input == data[10]['title']['$t']) {return 10}
-		else {return '11'}
+		var number = 0;
+		if (input == "") {return 0}
+		else {
+			do {number = number + 1} 
+			while (input != data[number]['title']['$t']);
+		}
+		return number;
 	}
-
-	console.log(data);
 
 	switch(w){
 		case 0:
 			document.getElementById("currentperiod").innerHTML = "It's a weekend, what are you doing checking the period?!";
-			document.getElementById("nextperiod").innerHTML = getperiod(currentpersonnumber, 1);
+			document.getElementById("nextperiod").innerHTML = getperiod(cpn, 1);
 			secnext = (time("24:00") - t) + time("8:10");
 			document.getElementById("secnextperiod").innerHTML = secnext;
 			break;
 		
 		case 1:
-			if (t < time("8:10")) {n = getperiod(currentpersonnumber, 1); secnext = time("8:10") - t}
-			else if (time("8:10") <= t && t < time("9:00")) {p = getperiod(currentpersonnumber, 1); n = getperiod(currentpersonnumber, 2); secnext = time("9:00") - t}
-			else if (time("9:00") <= t && t < time("9:50")) {p = getperiod(currentpersonnumber, 2); n = "Town Meeting"; secnext = time("9:50") - t} 
+			if (t < time("8:10")) {n = getperiod(cpn, 1); secnext = time("8:10") - t}
+			else if (time("8:10") <= t && t < time("9:00")) {p = getperiod(cpn, 1); n = getperiod(cpn, 2); secnext = time("9:00") - t}
+			else if (time("9:00") <= t && t < time("9:50")) {p = getperiod(cpn, 2); n = "Town Meeting"; secnext = time("9:50") - t} 
 			else if (time("9:50") <= t && t < time("10:10")) {p = "Town Meeting"; n = "Break"; secnext = time("10:10") - t}  
-			else if (time("10:10") <= t && t < time("10:30")) {p = "Break"; n = getperiod(currentpersonnumber, 3); secnext = time("10:30") - t}
-			else if (time("10:30") <= t && t < time("11:20")) {p = getperiod(currentpersonnumber, 3); n = getperiod(currentpersonnumber, 4); secnext = time("11:20") - t}
-			else if (time("11:20") <= t && t < time("12:10")) {p = getperiod(currentpersonnumber, 4); n = "Lunch"; secnext = time("12:10") - t}
-			else if (time("12:10") <= t && t < time("12:50")) {p = "Lunch"; n = getperiod(currentpersonnumber, 5); secnext = time("12:50") - t}
-			else if (time("12:50") <= t && t < time("13:40")) {p = getperiod(currentpersonnumber, 5); n = getperiod(currentpersonnumber, 6); secnext = time("13:40") - t}
-			else if (time("13:40") <= t && t < time("14:30")) {p = getperiod(currentpersonnumber, 6); n = getperiod(currentpersonnumber, 7); secnext = time("14:30") - t}
-			else if (time("14:30") <= t && t < time("15:20")) {p = getperiod(currentpersonnumber, 7); n = "End of school"; secnext = time("15:20") - t}
-			else {n = getperiod(currentpersonnumber, 1); secnext = (time("24:00") - t) + time("8:10")}
+			else if (time("10:10") <= t && t < time("10:30")) {p = "Break"; n = getperiod(cpn, 3); secnext = time("10:30") - t}
+			else if (time("10:30") <= t && t < time("11:20")) {p = getperiod(cpn, 3); n = getperiod(cpn, 4); secnext = time("11:20") - t}
+			else if (time("11:20") <= t && t < time("12:10")) {p = getperiod(cpn, 4); n = "Lunch"; secnext = time("12:10") - t}
+			else if (time("12:10") <= t && t < time("12:50")) {p = "Lunch"; n = getperiod(cpn, 5); secnext = time("12:50") - t}
+			else if (time("12:50") <= t && t < time("13:40")) {p = getperiod(cpn, 5); n = getperiod(cpn, 6); secnext = time("13:40") - t}
+			else if (time("13:40") <= t && t < time("14:30")) {p = getperiod(cpn, 6); n = getperiod(cpn, 7); secnext = time("14:30") - t}
+			else if (time("14:30") <= t && t < time("15:20")) {p = getperiod(cpn, 7); n = "End of school"; secnext = time("15:20") - t}
+			else {n = getperiod(cpn, 1); secnext = (time("24:00") - t) + time("8:10")}
 			document.getElementById("currentperiod").innerHTML = p;
 			document.getElementById("nextperiod").innerHTML = n;
 			document.getElementById("secnextperiod").innerHTML = secnext;
 			break;
 		
 		case 2:
-			if (t < time("8:10")) {n = getperiod(currentpersonnumber, 1); secnext = time("8:10") - t}
-			else if (time("8:10") <= t && t < time("9:50")) {p = getperiod(currentpersonnumber, 1); n = "Advisory"; secnext = time("9:50") - t}
+			if (t < time("8:10")) {n = getperiod(cpn, 1); secnext = time("8:10") - t}
+			else if (time("8:10") <= t && t < time("9:50")) {p = getperiod(cpn, 1); n = "Advisory"; secnext = time("9:50") - t}
 			else if (time("9:50") <= t && t < time("10:10")) {p = "Advisory"; n= "Break"; secnext = time("10:10") - t} 
-			else if (time("10:10") <= t && t < time("10:40")) {p = "Break"; n = getperiod(currentpersonnumber, 2); secnext = time("10:40") - t}
-			else if (time("10:40") <= t && t < time("11:40")) {p = getperiod(currentpersonnumber, 2); n = getperiod(currentpersonnumber, 6); secnext = time("11:40") - t}
-			else if (time("11:40") <= t && t < time("12:40")) {p = getperiod(currentpersonnumber, 6); n = "Lunch"; secnext = time("12:40") - t}
-			else if (time("12:40") <= t && t < time("13:40")) {p = "Lunch"; n = getperiod(currentpersonnumber, 7); secnext = time("13:40") - t}
-			else if (time("13:40") <= t && t < time("15:20")) {p = getperiod(currentpersonnumber, 7); n = "End of school"; secnext = time("15:20") - t}
-			else {n = getperiod(currentpersonnumber, 3); secnext = (time("24:00") - t) + time("8:10")}
+			else if (time("10:10") <= t && t < time("10:40")) {p = "Break"; n = getperiod(cpn, 2); secnext = time("10:40") - t}
+			else if (time("10:40") <= t && t < time("11:40")) {p = getperiod(cpn, 2); n = getperiod(cpn, 6); secnext = time("11:40") - t}
+			else if (time("11:40") <= t && t < time("12:40")) {p = getperiod(cpn, 6); n = "Lunch"; secnext = time("12:40") - t}
+			else if (time("12:40") <= t && t < time("13:40")) {p = "Lunch"; n = getperiod(cpn, 7); secnext = time("13:40") - t}
+			else if (time("13:40") <= t && t < time("15:20")) {p = getperiod(cpn, 7); n = "End of school"; secnext = time("15:20") - t}
+			else {n = getperiod(cpn, 3); secnext = (time("24:00") - t) + time("8:10")}
 			document.getElementById("currentperiod").innerHTML = p;
 			document.getElementById("nextperiod").innerHTML = n;
 			document.getElementById("secnextperiod").innerHTML = secnext;
 			break;
 
 		case 3:
-			if (t < time("8:10")) {n = getperiod(currentpersonnumber, 3); secnext = time("8:10") - t}
-			else if (time("8:10") <= t && t < time("9:50")) {p = getperiod(currentpersonnumber, 3); n = "Town Meeting"; secnext = time("9:50") - t}
+			if (t < time("8:10")) {n = getperiod(cpn, 3); secnext = time("8:10") - t}
+			else if (time("8:10") <= t && t < time("9:50")) {p = getperiod(cpn, 3); n = "Town Meeting"; secnext = time("9:50") - t}
 			else if (time("9:50") <= t && t < time("10:10")) {p = "Town Meeting"; n = "Break"; secnext = time("10:10") - t}
 			else if (time("10:10") <= t && t < time("10:40")) {p = "Break"; n = "Flex"; secnext = time("10:40") - t}
-			else if (time("10:40") <= t && t < time("11:40")) {p = "Flex"; n = getperiod(currentpersonnumber, 4); secnext = time("11:40") - t}
-			else if (time("11:40") <= t && t < time("12:40")) {p = getperiod(currentpersonnumber, 4); n = "Lunch"; secnext = time("12:40") - t}
-			else if (time("12:40") <= t && t < time("13:40")) {p = "Lunch"; n = getperiod(currentpersonnumber, 5); secnext = time("13:40") - t}
-			else if (time("13:40") <= t && t < time("15:20")) {p = getperiod(currentpersonnumber, 5); n = "End of school"; secnext = time("15:20") - t}
-			else {n = getperiod(currentpersonnumber, 2); secnext = (time("24:00") - t) + time("8:10")}
+			else if (time("10:40") <= t && t < time("11:40")) {p = "Flex"; n = getperiod(cpn, 4); secnext = time("11:40") - t}
+			else if (time("11:40") <= t && t < time("12:40")) {p = getperiod(cpn, 4); n = "Lunch"; secnext = time("12:40") - t}
+			else if (time("12:40") <= t && t < time("13:40")) {p = "Lunch"; n = getperiod(cpn, 5); secnext = time("13:40") - t}
+			else if (time("13:40") <= t && t < time("15:20")) {p = getperiod(cpn, 5); n = "End of school"; secnext = time("15:20") - t}
+			else {n = getperiod(cpn, 2); secnext = (time("24:00") - t) + time("8:10")}
 			document.getElementById("currentperiod").innerHTML = p;
 			document.getElementById("nextperiod").innerHTML = n;
 			document.getElementById("secnextperiod").innerHTML = secnext;
 			break;
 
 		case 4:
-			if (t < time("8:10")) {n = getperiod(currentpersonnumber, 2); secnext = time("8:10") - t}
-			else if (time("8:10") <= t && t < time("9:50")) {p = getperiod(currentpersonnumber, 2); n = "Advisory"; secnext = time("9:50") - t}
+			if (t < time("8:10")) {n = getperiod(cpn, 2); secnext = time("8:10") - t}
+			else if (time("8:10") <= t && t < time("9:50")) {p = getperiod(cpn, 2); n = "Advisory"; secnext = time("9:50") - t}
 			else if (time("9:50") <= t && t < time("10:10")) {p = "Advisory"; n = "Break"; secnext = time("10:10") - t}
-			else if (time("10:10") <= t && t < time("10:40")) {p = "Break"; n = getperiod(currentpersonnumber, 1); secnext = time("10:40") - t}
-			else if (time("10:40") <= t && t < time("11:40")) {p = getperiod(currentpersonnumber, 1); n = getperiod(currentpersonnumber, 7); secnext = time("11:40") - t}
-			else if (time("11:40") <= t && t < time("12:40")) {p = getperiod(currentpersonnumber, 7); n = "Lunch"; secnext = time("12:40") - t}
-			else if (time("12:40") <= t && t < time("13:40")) {p = "Lunch"; n = getperiod(currentpersonnumber, 6); secnext = time("13:40") - t}
-			else if (time("13:40") <= t && t < time("15:20")) {p = getperiod(currentpersonnumber, 6); n = "End of school"; secnext = time("15:20") - t}
-			else {n = getperiod(currentpersonnumber, 4); secnext = (time("24:00") - t) + time("8:10")}
+			else if (time("10:10") <= t && t < time("10:40")) {p = "Break"; n = getperiod(cpn, 1); secnext = time("10:40") - t}
+			else if (time("10:40") <= t && t < time("11:40")) {p = getperiod(cpn, 1); n = getperiod(cpn, 7); secnext = time("11:40") - t}
+			else if (time("11:40") <= t && t < time("12:40")) {p = getperiod(cpn, 7); n = "Lunch"; secnext = time("12:40") - t}
+			else if (time("12:40") <= t && t < time("13:40")) {p = "Lunch"; n = getperiod(cpn, 6); secnext = time("13:40") - t}
+			else if (time("13:40") <= t && t < time("15:20")) {p = getperiod(cpn, 6); n = "End of school"; secnext = time("15:20") - t}
+			else {n = getperiod(cpn, 4); secnext = (time("24:00") - t) + time("8:10")}
 			document.getElementById("currentperiod").innerHTML = p;
 			document.getElementById("nextperiod").innerHTML = n;
 			document.getElementById("secnextperiod").innerHTML = secnext;
 			break;
 
 		case 5:
-			if (t < time("8:10")) {n = getperiod(currentpersonnumber, 4); secnext = time("8:10") - t}
-			else if (time("8:10") <= t && t < time("9:50")) {p = getperiod(currentpersonnumber, 4); n = "Town Meeting"; secnext = time("9:50") - t}
+			if (t < time("8:10")) {n = getperiod(cpn, 4); secnext = time("8:10") - t}
+			else if (time("8:10") <= t && t < time("9:50")) {p = getperiod(cpn, 4); n = "Town Meeting"; secnext = time("9:50") - t}
 			else if (time("9:50") <= t && t < time("10:10")) {p = "Town Meeting"; n = "Break"; secnext = time("10:10") - t}
 			else if (time("10:10") <= t && t < time("10:40")) {p = "Break"; n = "Flex"; secnext = time("10:40") - t}
-			else if (time("10:40") <= t && t < time("11:30")) {p = "Flex"; n = getperiod(currentpersonnumber, 3); secnext = time("11:30") - t}
-			else if (time("11:30") <= t && t < time("12:30")) {p = getperiod(currentpersonnumber, 3); n = "Lunch"; secnext = time("12:30") - t}
-			else if (time("12:30") <= t && t < time("13:30")) {p = "Lunch"; n = getperiod(currentpersonnumber, 5); secnext = time("13:30") - t}
-			else if (time("13:30") <= t && t < time("14:30")) {p = getperiod(currentpersonnumber, 5); n = "End of school"; secnext = time("14:30") - t}
-			else {n = getperiod(currentpersonnumber, 1); secnext = ((time("24:00") * 3) - t + time("8:10"))}
+			else if (time("10:40") <= t && t < time("11:30")) {p = "Flex"; n = getperiod(cpn, 3); secnext = time("11:30") - t}
+			else if (time("11:30") <= t && t < time("12:30")) {p = getperiod(cpn, 3); n = "Lunch"; secnext = time("12:30") - t}
+			else if (time("12:30") <= t && t < time("13:30")) {p = "Lunch"; n = getperiod(cpn, 5); secnext = time("13:30") - t}
+			else if (time("13:30") <= t && t < time("14:30")) {p = getperiod(cpn, 5); n = "End of school"; secnext = time("14:30") - t}
+			else {n = getperiod(cpn, 1); secnext = ((time("24:00") * 3) - t + time("8:10"))}
 			document.getElementById("currentperiod").innerHTML = p;
 			document.getElementById("nextperiod").innerHTML = n;
 			document.getElementById("secnextperiod").innerHTML = secnext;
@@ -133,7 +126,7 @@ function whatPeriod(data) {
 		
 		case 6:
 			document.getElementById("currentperiod").innerHTML = "It's a weekend, what are you doing checking the period?!";
-			document.getElementById("nextperiod").innerHTML = getperiod(currentpersonnumber, 1);
+			document.getElementById("nextperiod").innerHTML = getperiod(cpn, 1);
 			secnext = ((time("24:00") * 2) - t + time("8:10"));
 			document.getElementById("secnextperiod").innerHTML = secnext;
 			break;
